2e91853e23f6433f8807e70b6ca96b8b5f4b2c7f
Njoki Kamau <njokifaith61@gmail.com>
2020-05-27 15:30:00 +0300

    changing events to be jquery format

--- js/script.js
+++ js/script.js
@@ -1,102 +1,97 @@
 //Match variables with user inputs
+var totalCostArray = [];
+//Only global variable in code
 $(document).ready(function () {
-
-
-//Declaring and defining variables and their values
-
-
-
-var crust=document.getElementById("crust");
-var toppings=document.getElementById("topping");
-var size=document.getElementById("size");
-var type=document.getElementById("type")
-var total
-
-//Constructor function for basic addition
-
+  //Constructor
 function GetPizzaOrder( type, size, topping, crust) {
     this.type = type.value;
     this.size = parseInt(size).value;
     this.topping = parseInt(topping).value;
     this.crust = parseInt(crust).value;
     this.delivery= 100;
-    this.total= this.size + this.topping +this.crust + this.delivery;
-    console.log("Your cost so far is "+this.total+");
  }
-
-
-
- switch(size){
-   case "Family";
-   price= 2500;
-   break;
-   case "Large";
-   price= 1000;
-   break;
-   case "Medium";
-   price= 700;
-   break;
-   case "Small";
-   price= 450;
-   break;
-   default:
-   alert('Please choose a size')
+//Prototype
+ PizzaOrder.prototype.pizzaCost = function () {
+   if (this.size === "small") {
+     this.price += 450;
+   } else if (this.size === "medium.") {
+     this.price += 700;
+   } else if (this.size === "large") {
+     this.price += 1000;
+   } else if (this.size === "family") {
+   this.price += 2500;
+   }
+ } else if (this.size === "") {
+ console.log('Please choose a size');
  }
- switch(topping){
-   case "Guacomole";
-   price=100 ;
-   break;
-   case "Sausage";
-   price= 100;
-   break;
-   case "Bacon";
-   price= 100;
-   break;
-   case "Mushroom";
-   price= 100;
-   break;
-   default:
-   alert('Please choose a topping')
+   if (this.topping === "Sausage") {
+     this.price += 100;
+   } else if (this.topping === "Guacomole") {
+     this.price += 150;
+   } else if (this.topping=== "Mushroom") {
+     this.price += 50;
+   }
+ } else if (this.topping=== "") {
+   console.log('Please choose a topping');
  }
- switch(crust){
-   case "Regular";
-   price=0 ;
-   break;
-   case "Stuffed";
-   price= 150;
-   break;
-   case "Crispy";
-   price= 100;
-   break;
-   default:
-   alert('Please choose a crust')
+   if (this.crust === "crispy") {
+     this.price += 100;
+   } else if (this.crust === "stuffed") {
+     this.price += 150;
+   } else if (this.crust=== "regular") {
+     this.price += 50;
+   }
+ } else if (this.crust=== "") {
+    console.log('Please choose a crust');
  }
-
-PizzaOrder.prototype.totalCosts = function() {
-  return this.size +this.topping + this.crust + this.delivery;
+   this.price += this.size;
+   this.price += this.topping;
+   this.price += this.crust;
+   this.price += this.delivery;
+   return this.total;
+ }
 }
+});
+
 
 //Add to Cart button
-document.getElementById("addToCart").addEventListener("click", function GetPizzaOrder( type, size, topping, crust) {
+
+$("addToCart").click( function(){
     this.type = type.value;
     this.size = parseInt(size).value;
     this.topping = parseInt(topping).value;
     this.crust = parseInt(crust).value;
     this.delivery= 100;
-    this.total= this.size + this.topping +this.crust + this.delivery;
+    return this.total;
  });
+
+//Declaring and defining variables and their values
+
+
+
+
+//Constructor function for basic addition of costs
+
+
+
+//Selections
+
+
+//Prototype for adding costs
 
 var pizzaAdd2 = new PizzaOrder { type, size, topping, crust};
 
+//What happens when pizza is added
 function pizzaAdd(){
   new PizzaOrder = pizzaAdd2.size +pizzaAdd2.topping + pizzaAdd2.crust + pizzaAdd2.delivery};
 }
-
-document.getElementById("addPizza").addEventListener("click", function() {
+//Add Pizza Button
+  $("#addPizza").click(function function() {
    pizzaAdd2.total = pizzaAdd2.size +pizzaAdd2.topping + pizzaAdd2.crust + pizzaAdd2.delivery});
 
    let finalTotal= pizzaAdd2.total + this.total;
-//Add Pizza button
+
+//Checkout  Button and functions
 
 function checkoutTotal(){
   alert("Your order total is Ksh " + parseInt(finaTotal);
@@ -104,12 +99,12 @@ function checkoutTotal(){
 
 
 }
-document.getElementById("checkout").addEventListener("click", function CheckoutTotal(){
+("#checkout").click(function(){
   alert("Your final total is Ksh " +parseInt(finaTotal)+ "Please fill in your details for delivery");
 });
 
 //Delivery Address finalization and order confirmation
-document.getElementById("confirm").addEventListener("click",(function(event){
+  $("#confirm").click(function(){
       event.preventDefault();
 
       let person = $("input#name").val();
@@ -118,7 +113,7 @@ document.getElementById("confirm").addEventListener("click",(function(event){
 
       if ($("input#name").val() && $("input#phone").val() && $("input#address").val()!=""){
 
-        alert(person+", We have recieved your order and it will be delivered to you at "+address+ ".It will cost you "+finalTotal);
+        document.getElementById("confirm").innerHTML = person+", We have recieved your order and it will be delivered to you at "+address+ ".It will cost you "+finalTotal);
       }
       else {
         alert("Please fill in the details for delivery!");
