a05500cb61f0e10cd114a309c442c4db10a25e29
Njoki Kamau <njokifaith61@gmail.com>
2020-05-29 03:53:34 +0300

    resolve bugs and make buttons work

--- js/script.js
+++ js/script.js
@@ -1,135 +1,92 @@
-//Match variables with user inputs
-var totalCostArray = [];
-//Only global variable in code
-$(document).ready(function () {
-  //Constructor
-function GetPizzaOrder( type, size, topping, crust) {
-    this.type = type.value;
-    this.size = parseInt(size).value;
-    this.topping = parseInt(topping).value;
-    this.crust = parseInt(crust).value;
-    this.delivery= 100;
- }
-//Prototype
- PizzaOrder.prototype.pizzaCost = function () {
-   if (this.size === "small") {
-     this.price += 450;
-   } else if (this.size === "medium.") {
-     this.price += 700;
-   } else if (this.size === "large") {
-     this.price += 1000;
-   } else if (this.size === "family") {
-   this.price += 2500;
-   }
- } else if (this.size === "") {
- console.log('Please choose a size');
- }
-   if (this.topping === "Sausage") {
-     this.price += 100;
-   } else if (this.topping === "Guacomole") {
-     this.price += 150;
-   } else if (this.topping=== "Mushroom") {
-     this.price += 50;
-   }
- } else if (this.topping=== "") {
-   console.log('Please choose a topping');
- }
-   if (this.crust === "crispy") {
-     this.price += 100;
-   } else if (this.crust === "stuffed") {
-     this.price += 150;
-   } else if (this.crust=== "regular") {
-     this.price += 50;
-   }
- } else if (this.crust=== "") {
-    console.log('Please choose a crust');
- }
-   this.price += this.size;
-   this.price += this.topping;
-   this.price += this.crust;
-   this.price += this.delivery;
-   return this.total;
- }
-}
-});
+$(document).ready(function(){
+var pizzas= [];
 
-
-//Add to Cart button
-
-$("addToCart").click( function(){
-    this.type = type.value;
-    this.size = parseInt(size).value;
-    this.topping = parseInt(topping).value;
-    this.crust = parseInt(crust).value;
-    this.delivery= 100;
-    return this.total;
- });
-
-//Declaring and defining variables and their values
-
-
-
-
-//Constructor function for basic addition of costs
-
-
-
-//Selections
-
-
-//Prototype for adding costs
-
-var pizzaAdd2 = new PizzaOrder { type, size, topping, crust};
-
-//What happens when pizza is added
-function pizzaAdd(){
-  new PizzaOrder = pizzaAdd2.size +pizzaAdd2.topping + pizzaAdd2.crust + pizzaAdd2.delivery};
-}
-//Add Pizza Button
-  $("#addPizza").click(function function() {
-   pizzaAdd2.total = pizzaAdd2.size +pizzaAdd2.topping + pizzaAdd2.crust + pizzaAdd2.delivery});
-
-   let finalTotal= pizzaAdd2.total + this.total;
-
-//Checkout  Button and functions
-
-function checkoutTotal(){
-  alert("Your order total is Ksh " + parseInt(finaTotal);
-};
-
-
-}
-("#checkout").click(function(){
-  alert("Your final total is Ksh " +parseInt(finaTotal)+ "Please fill in your details for delivery");
-});
-
-//Delivery Address finalization and order confirmation
-  $("#confirm").click(function(){
+  $("#addToCart-btn").click(function(event){
       event.preventDefault();
-
-      let person = $("input#name").val();
-      let phone = $("input#phone").val();
-      let address = $("input#address").val();
-
-      if ($("input#name").val() && $("input#phone").val() && $("input#address").val()!=""){
-
-        document.getElementById("confirm").innerHTML = person+", We have recieved your order and it will be delivered to you at "+address+ ".It will cost you "+finalTotal);
+      var type= document.getElementById("type").value;
+      var size= document.getElementById("size").value;
+      var topping= document.getElementById("topping").value;
+      var crust= document.getElementById("crust").value;
+  //Constructor
+      function Pizza(type, size, topping, crust){
+        this.type = type;
+        this.size = size;
+        this.topping= topping;
+        this.crust = crust;
       }
-      else {
-        alert("Please fill in the details for delivery!");
-        $(".delivery").show();
-        $("button#confirmation").show();
+      //Attach prototype to function adding total
+    Pizza.prototype.calculateCosts = function(){
+
+      var price;
+      var tprice;
+      var cprice;
+
+
+      if (this.size === "small") {
+         price = 450;
+      } else if (this.size === "medium") {
+         price = 700;
+      } else if (this.size === "large") {
+         price = 1000;
+      } else if (this.size === "family") {
+         price = 2500;
+      } else if (this.size === "") {
+        console.log("Please choose a size");
       }
-    });
-   event.preventDefault();
+      if (this.topping === "Sausage") {
+        tprice = 100;
+      } else if (this.topping === "Guacomole") {
+        tprice = 150;
+      } else if (this.topping=== "Mushroom") {
+        tprice = 50;
+      } else if (this.topping=== "") {
+        console.log('Please choose a topping');
+      }
+      if (pizzaOrder.crust === "Crispy") {
+        cprice = 100;
+      } else if (this.crust === "Stuffed") {
+        cprice = 150;
+      } else if (this.crust=== "Regular") {
+        cprice = 50;
+      } else if (this.crust=== "") {
+        console.log('Please choose a crust');
+      }
+      let total = parseInt(price) + parseInt(tprice) + parseInt(cprice);
+      return total;
+    }
+    //Creation of Pizza
+
+
+      let pizzaOrder= new Pizza(type, size, topping, crust);
+
+      pizzas.push(pizzaOrder);
+      var pizzaCost = pizzaOrder.calculateCosts();
+
+      console.log(pizzaCost);
+
+      alert("Your current cost is Kshs " +pizzaCost+ ".Feel free to add more pizzas to your cart.")
+//Array
+
+
+
   });
+ $("#checkout-btn").click(function(){
+    var checkoutTotal= 0;
 
+    pizzas.forEach(function(pizza){
+
+    checkoutTotal+=pizza.calculateCosts();
+
+
+    });
+    console.log(checkoutTotal);
+    alert("Your cart items add up to Kshs." +checkoutTotal+ ". You can now proceed, add delivery address, and confirm order" )
+  });
+  $("#confirm-btn").click(function(){
+
+      var name = document.getElementById("name").value;
+      var phone= document.getElementById("phone").value;
+      var address= document.getElementById("address").value;
+   alert( "Thank you for being our customer. Your order will be deliverd within an hour." )
+  });
 });
-
-
-
-//Proceed to Checkout button
-
-
-
-//Confirm orders
